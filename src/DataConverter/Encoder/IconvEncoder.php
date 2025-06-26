ccb1e2ee18f745487fa035879a8f27d8f3d63a97
Adiwit Co., Ltd <info@adiwit.co.th>
2025-06-26 22:19:57 +0700

    Handles "iconv(): Detected an illegal character in input string" errors
    For example "š10121006"

--- src/DataConverter/Encoder/IconvEncoder.php
+++ src/DataConverter/Encoder/IconvEncoder.php
@@ -6,6 +6,6 @@ class IconvEncoder implements EncoderInterface
 {
     public function encode(string $string, string $fromEncoding, string $toEncoding): string
     {
-        return iconv($fromEncoding, $toEncoding, $string);
+        return iconv($fromEncoding, "$toEncoding//IGNORE", $string);
     }
 }
